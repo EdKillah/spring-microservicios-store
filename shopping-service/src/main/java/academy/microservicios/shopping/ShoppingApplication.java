6f0b6a39e4db6df0dcbf33633afdb73c5433fc4a
Eduard Jimenez <eduard.jimenez@mail.escuelaing.edu.co>
2021-07-19 10:20:23 -0500

    actualizando shopping-service.yml para usar hystrix

--- shopping-service/src/main/java/academy/microservicios/shopping/ShoppingApplication.java
+++ shopping-service/src/main/java/academy/microservicios/shopping/ShoppingApplication.java
@@ -3,10 +3,14 @@ package academy.microservicios.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ShoppingApplication {
 
